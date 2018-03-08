455af1fe00f400957c40f3bd32f89f8f550dc1ee
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2018-03-08 13:35:06 -0500

    Add files via upload
    Allows the spawning of prefabs under resources folder

---
+++ AssetSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetSpawner : MonoBehaviour {
+
+    public Vector3 position;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            createPreFab();
+        }
+    }
+
+    void createPreFab()
+    {
+        GameObject myPreFabClone = Resources.Load("Box") as GameObject;
+        Instantiate(myPreFabClone, position, Quaternion.identity);
+    }
+}
